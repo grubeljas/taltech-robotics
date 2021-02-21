71957474bb87a62db2e7580e24b00ac1a7c2670a
pagrub <pagrub@ttu.ee>
2021-02-21 21:37:35 +0200

    L1 realism maybe?

--- L1/robot.py
+++ L1/robot.py
@@ -1,5 +1,6 @@
 """L1."""
 import PiBot
+import math
 
 
 class Robot:
@@ -9,7 +10,9 @@ class Robot:
         """Initialize class."""
         self.robot = PiBot.PiBot()
         self.shutdown = False
-
+        self.simulation = self.robot.is_simulation()
+        if self.simulation:
+            self.wheel_size = self.robot.WHEEL_DIAMETER * math.pi
         self.leftmost_line_sensor = 0
         self.second_left_line_sensor = 0
         self.center_left_line_sensor = 0
@@ -17,6 +20,8 @@ class Robot:
         self.rightmost_line_sensor = 0
         self.second_right_line_sensor = 0
         self.center_right_line_sensor = 0
+        self.all = []
+        self.i = 0
 
         self.line_direction = None
 
@@ -24,7 +29,17 @@ class Robot:
         self.right_wheel_speed = 0
 
         self.state = "Finding the line"
+        if self.simulation:
+            self.prev_dir = ""
         self.no_line_counter = 0
+
+        self.right_encoder = 0
+        self.left_encoder = 0
+        self.time = 0
+
+        self.previous_right = 0
+        self.previous_left = 0
+        self.previous_time = 0
 
     def set_robot(self, robot: PiBot.PiBot()) -> None:
         """
@@ -59,6 +74,7 @@ class Robot:
            straight: Robot is on the line (i.e., the robot should not turn to stay on the line)
            left: Line is on the left (i.e., the robot should turn left to reach the line again)
         """
+        self.prev_dir = self.line_direction
         if self.center_left_line_sensor < 400 or self.center_right_line_sensor < 400:
             self.line_direction = "straight"
         elif self.leftmost_line_sensor < 400 or self.second_left_line_sensor < 400:
@@ -73,8 +89,11 @@ class Robot:
         """Instruction for robot to follow the line."""
         line_direction = self.get_line_direction()
         if line_direction == "straight":
-            self.left_wheel_speed = 8
-            self.right_wheel_speed = 8
+            if self.simulation and line_direction == self.prev_dir:
+                if self.get_left_velocity() != self.get_right_velocity():
+                    self.i += 1
+            self.left_wheel_speed = 8 + self.i
+            self.right_wheel_speed = 8 + self.i
         elif line_direction == "right":
             self.left_wheel_speed = 8
             self.right_wheel_speed = -8
@@ -90,9 +109,41 @@ class Robot:
         elif self.no_line_counter > 50:
             self.shutdown = True
 
+    def get_left_velocity(self) -> float:
+        """
+        Return the current left wheel velocity.
+
+        Returns:
+          The current wheel translational velocity in meters per second.
+        """
+        velocity = 0
+        if self.robot.get_time() != 0:
+            velocity = (self.left - self.previous_left) / 360 * self.wheel_size / (self.time - self.previous_time)
+        return velocity
+
+    def get_right_velocity(self) -> float:
+        """
+        Return the current right wheel velocity.
+
+        Returns:
+          The current wheel translational velocity in meters per second.
+        """
+        velocity = 0
+        if self.robot.get_time() != 0:
+            velocity = (self.right - self.previous_right) / 360 * self.wheel_size / (self.time - self.previous_time)
+        return velocity
+
     def sense(self):
         """Sense - gets all the information."""
         print("Getting info!")
+        self.previous_right = self.right
+        self.previous_left = self.left
+        self.previous_time = self.time
+
+        self.right = self.robot.get_right_wheel_encoder()
+        self.left = self.robot.get_left_wheel_encoder()
+        self.time = self.robot.get_time()
+
         self.leftmost_line_sensor = self.robot.get_leftmost_line_sensor()
         self.second_left_line_sensor = self.robot.get_second_line_sensor_from_left()
         self.center_left_line_sensor = self.robot.get_third_line_sensor_from_left()
@@ -100,6 +151,9 @@ class Robot:
         self.rightmost_line_sensor = self.robot.get_rightmost_line_sensor()
         self.second_right_line_sensor = self.robot.get_second_line_sensor_from_right()
         self.center_right_line_sensor = self.robot.get_third_line_sensor_from_right()
+
+        self.all = self.robot.get_left_line_sensors()
+        self.all += [self.center_right_line_sensor, self.second_right_line_sensor, self.rightmost_line_sensor]
 
     def plan(self):
         """Plan - decides what to do based on the information."""
@@ -127,6 +181,7 @@ class Robot:
             print(f'The time is {self.robot.get_time()}!')
             self.sense()
             print(f"Left sensor: {self.center_left_line_sensor}, Right sensor: {self.center_right_line_sensor}")
+            print(f"{self.all}")
             self.plan()
             self.act()
             self.robot.sleep(0.05)
