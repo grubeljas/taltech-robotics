6de6e40691116aa0e5c8c834dc3f142945c02979
pagrub <pagrub@ttu.ee>
2021-02-27 16:45:13 +0200

    l3

--- L3/robot.py
+++ L3/robot.py
@@ -18,29 +18,23 @@ class Robot:
         self.second_right_line_sensor = 0
         self.center_right_line_sensor = 0
 
-        self.prev_rear_left_straight = 0
-        self.prev_rear_left_diagonal = 0
-        self.prev_rear_left_side = 0
+        self.front_middle_laser = 0
+        self.front_right_laser = 0
+        self.right_ir = 0
+        self.right_ir_initial = 0
 
-        self.prev_rear_right_straight = 0
-        self.prev_rear_right_diagonal = 0
-        self.prev_rear_right_side = 0
-
-        self.rear_left_straight_ir = self.robot.get_rear_left_straight_ir()
-        self.rear_left_diagonal_ir = self.robot.get_rear_left_diagonal_ir()
-        self.rear_left_side_ir = self.robot.get_rear_left_side_ir()
-
-        self.rear_right_straight_ir = self.robot.get_rear_right_straight_ir()
-        self.rear_right_diagonal_ir = self.robot.get_rear_right_diagonal_ir()
-        self.rear_right_side_ir = self.robot.get_rear_right_side_ir()
-
+        self.crossroad_turn = "left"
+        self.starting_orientation = None
+        self.current_orientation = None
 
         self.left_wheel_speed = 0
         self.right_wheel_speed = 0
+
         self.state = "Finding the line"
         self.no_line_counter = 0
-        self.bypass_counter = 0
 
+        self.obstacle_phase = "Turning away"
+        self.got_off_the_line = False
 
     def set_robot(self, robot: PiBot.PiBot()) -> None:
         """
@@ -89,7 +83,6 @@ class Robot:
         else:
             self.turn_left()
 
-
     def get_line_direction(self):
         """
         Return the direction of the line based on sensor readings.
@@ -99,75 +92,82 @@ class Robot:
            straight: Robot is on the line (i.e., the robot should not turn to stay on the line)
            left: Line is on the left (i.e., the robot should turn left to reach the line again)
         """
-        if self.center_left_line_sensor < 400 and self.center_right_line_sensor < 400:
+        line_direction = "absent"
+
+        if self.center_left_line_sensor < 200 and self.center_right_line_sensor < 200:
             line_direction = "straight"
-        elif self.leftmost_line_sensor < 400 or self.second_left_line_sensor < 400:
-            line_direction = "left"
-        elif self.rightmost_line_sensor < 400 or self.second_right_line_sensor < 400:
-            line_direction = "right"
-        elif self.center_left_line_sensor < 400:
-            line_direction = "leaning left"
-        elif self.center_right_line_sensor < 400:
+        elif self.second_right_line_sensor < 200 or self.center_right_line_sensor < 200:
             line_direction = "leaning right"
-        else:
-            line_direction = "absent"
+        elif self.second_left_line_sensor < 200 or self.center_left_line_sensor < 200:
+            line_direction = "leaning left"
+        elif self.rightmost_line_sensor < 200:
+            line_direction = "hard right"
+        elif self.leftmost_line_sensor < 200:
+            line_direction = "hard left"
+
         return line_direction
 
     def follow_the_line(self):
         """Instruction for robot to follow the line."""
         line_direction = self.get_line_direction()
+
         if line_direction == "straight":
             self.go_straight()
-        elif line_direction == "right":
+        if line_direction == "leaning right":
             self.gradual_turn_right()
-        elif line_direction == "left":
+        if line_direction == "leaning left":
             self.gradual_turn_left()
-        elif line_direction == "leaning left":
-            self.gradual_turn_left()
-        elif line_direction == "leaning right":
-            self.gradual_turn_right()
-        elif line_direction == "absent":
+        if line_direction == "hard right":
+            self.turn_right()
+        if line_direction == "hard left":
+            self.turn_left()
+
+        if line_direction == "absent":
             self.no_line_counter += 1
-            print(f"No line counter = {self.no_line_counter}")
-        if self.rear_right_straight_ir < 400:
-            self.turn_left()
-            self.state = "Bypassing"
-
-        if self.no_line_counter > 0 and line_direction != "absent":
-            self.no_line_counter = 0
-        elif self.no_line_counter > 50:
-            self.state = "Finding the line"
-
-    def do_bypass(self):
-        """Ride around tha object on the left."""
-        print("Bypass")
-        self.bypass_counter += 1
-        if self.rear_left_diagonal_ir < 0.07:
-            self.go_straight()
         else:
-            self.turn_left()
+            self.no_line_counter = 0
+
+        if self.no_line_counter > 500:
             self.state = "Finding the line"
+
+        if self.front_middle_laser < 0.08:
+            self.state = "Avoiding obstacle"
+            print(self.state)
+            self.starting_orientation = self.current_orientation
+
+    def avoid_obstacle(self):
+        """Avoid obstacle."""
+        if self.obstacle_phase == "Turning away":
+            if abs(self.starting_orientation - self.current_orientation) > 85:
+                self.obstacle_phase = "Moving around"
+                self.right_ir_initial = self.right_ir
+            else:
+                self.turn_left()
+
+        if self.obstacle_phase == "Moving around":
+            print(self.right_ir)
+            if self.right_ir_initial - 20 < self.right_ir < self.right_ir_initial + 20:
+                self.go_straight()
+            elif self.right_ir < self.right_ir_initial - 100:
+                self.go_straight()
+            elif self.right_ir < self.right_ir_initial - 20:
+                self.gradual_turn_right()
+            elif self.right_ir > self.right_ir_initial + 20:
+                self.gradual_turn_left()
+            if self.get_line_direction() != "absent":
+                self.obstacle_phase = "Back on track"
+                self.starting_orientation = self.current_orientation
+
+        if self.obstacle_phase == "Back on track":
+            self.turn_left()
+            if self.get_line_direction() == "hard right":
+                self.state = "Following the line"
+                self.obstacle_phase = "Turning away"
+
+        pass
 
     def sense(self):
         """Sense - gets all the information."""
-        if self.robot.get_time() % 0.1 < 0.05:
-            print(f'The time is {self.robot.get_time()}!')
-            self.prev_rear_left_straight = self.rear_left_straight_ir
-            self.prev_rear_left_diagonal = self.rear_left_diagonal_ir
-            self.prev_rear_left_side = self.rear_left_side_ir
-            self.prev_rear_right_straight = self.rear_right_straight_ir
-            self.prev_rear_right_diagonal = self.rear_right_diagonal_ir
-            self.prev_rear_right_side = self.rear_right_side_ir
-            print(f"INFRARED{self.robot.get_rear_irs()}")
-
-        self.rear_left_straight_ir = self.robot.get_rear_left_straight_ir()
-        self.rear_left_diagonal_ir = self.robot.get_rear_left_diagonal_ir()
-        self.rear_left_side_ir = self.robot.get_rear_left_side_ir()
-
-        self.rear_right_straight_ir = self.robot.get_rear_right_straight_ir()
-        self.rear_right_diagonal_ir = self.robot.get_rear_right_diagonal_ir()
-        self.rear_right_side_ir = self.robot.get_rear_right_side_ir()
-
         self.leftmost_line_sensor = self.robot.get_leftmost_line_sensor()
         self.second_left_line_sensor = self.robot.get_second_line_sensor_from_left()
         self.center_left_line_sensor = self.robot.get_third_line_sensor_from_left()
@@ -176,14 +176,20 @@ class Robot:
         self.second_right_line_sensor = self.robot.get_second_line_sensor_from_right()
         self.center_right_line_sensor = self.robot.get_third_line_sensor_from_right()
 
+        self.current_orientation = self.robot.get_rotation()
+
+        self.front_middle_laser = self.robot.get_front_middle_laser()
+        self.front_right_laser = self.robot.get_front_right_laser()
+        self.right_ir = self.robot.get_rear_right_side_ir()
+
     def plan(self):
         """Plan - decides what to do based on the information."""
         if self.state == "Finding the line":
             self.find_the_line()
         elif self.state == "Following the line":
             self.follow_the_line()
-        elif self.state == "Bypassing":
-            self.do_bypass()
+        elif self.state == "Avoiding obstacle":
+            self.avoid_obstacle()
 
         if self.shutdown:
             self.left_wheel_speed = 0
@@ -201,9 +207,9 @@ class Robot:
         This loop is expected to call sense, plan, act methods cyclically.
         """
         while not self.shutdown:
-
+            #  print(f'The time is {self.robot.get_time()}!')
             self.sense()
-            #print(f"Left sensor: {self.center_left_line_sensor}, Right sensor: {self.center_right_line_sensor}")
+            #  print(f"Left sensor: {self.center_left_line_sensor}, Right sensor: {self.center_right_line_sensor}")
             self.plan()
             self.act()
             self.robot.sleep(0.05)
