2d281b64b005ce4e4307e5ded7ff96cbc18aa8b9
pagrub <pagrub@ttu.ee>
2021-06-07 22:03:55 +0300

    O1

--- S2/robot.py
+++ S2/robot.py
@@ -200,7 +200,6 @@ class Robot:
         """Set the rotation of the middle of the to closest balls."""
         if self.blue_ball_angle is None or self.red_ball_angle is None:
             self.act(0, 0)
-            self.shutdown = True
             return None
         angle = (self.blue_ball_angle + self.red_ball_angle) / 2
         diff = abs(self.rotation - angle)
